b03d91f7fccc8286f3a853f6128d135c2f872218
emilienbuguet <emilien.buguet@epitech.eu>
2023-11-14 18:00:00 +0100

    feat(lib): service auth

---
+++ auth/auth.go
@@ -0,0 +1,38 @@
+package auth
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/go-chi/jwtauth/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func NewTokenAuth() *jwtauth.JWTAuth {
+	tokenAuth := jwtauth.New("HS256", []byte(os.Getenv("JWT_SECRET")), nil)
+	return tokenAuth
+}
+
+func CreateToken(claims map[string]interface{}) (string, error) {
+	_, token, err := NewTokenAuth().Encode(claims)
+	return token, err
+}
+
+func VerifyToken(tokenString string) bool {
+	token, err := jwtauth.VerifyToken(NewTokenAuth(), tokenString)
+	if err != nil || token == nil {
+		return false
+	}
+	return true
+}
+
+func HashPassword(password string) string {
+	salt, _ := strconv.Atoi(os.Getenv("SALT"))
+	bytes, _ := bcrypt.GenerateFromPassword([]byte(password), salt)
+	return string(bytes)
+}
+
+func CheckPassword(password string, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
