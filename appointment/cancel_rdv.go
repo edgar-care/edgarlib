ea8ab68eed73820620ce38b6902a4ad011b4bb35
ThomasDarrieumerlou <thomas.darrieumerlou@epitech.eu>
2024-03-19 18:16:13 +0000

    fix(rdv): error when i try to book an appointment, checking the appointment is real

--- appointment/cancel_rdv.go
+++ appointment/cancel_rdv.go
@@ -24,6 +24,7 @@ func CancelRdv(id string, reason string) CancelRdvResponse {
 	}
 
 	var appointment_status = graphql.AppointmentStatusCanceled
+<<<<<<< HEAD
 	_, err = graphql.UpdateRdv(context.Background(), gqlClient, id, rdv.GetRdvById.Id_patient, rdv.GetRdvById.Doctor_id, rdv.GetRdvById.Start_date, rdv.GetRdvById.End_date, reason, appointment_status, rdv.GetRdvById.Session_id)
 	if err != nil {
 		return CancelRdvResponse{Reason: "", Code: 500, Err: errors.New("unable to update appointment")}
@@ -31,6 +32,9 @@ func CancelRdv(id string, reason string) CancelRdvResponse {
 
 	var new_appointment_status = graphql.AppointmentStatusOpened
 	_, err = graphql.CreateRdv(context.Background(), gqlClient, "", rdv.GetRdvById.Doctor_id, rdv.GetRdvById.Start_date, rdv.GetRdvById.End_date, new_appointment_status, "")
+=======
+	_, err = graphql.UpdateRdv(context.Background(), gqlClient, id, "", rdv.GetRdvById.Doctor_id, rdv.GetRdvById.Start_date, rdv.GetRdvById.End_date, reason, appointment_status, rdv.GetRdvById.Session_id)
+>>>>>>> 8496ff2 (fix)
 	if err != nil {
 		return CancelRdvResponse{Reason: "", Code: 500, Err: errors.New("unable to update appointment")}
 	}
--- appointment/edit_doctor_appointment.go
+++ appointment/edit_doctor_appointment.go
@@ -33,7 +33,12 @@ func UpdateDoctorAppointment(newAppointmentId string, appointmentId string) Upda
 	if err != nil {
 		return UpdateDoctorAppointmentStruct{Rdv: model.Rdv{}, Patient: model.Patient{}, Code: 400, Err: errors.New("id does not correspond to an appointment")}
 	}
+<<<<<<< HEAD
 	updatedRdv, err := graphql.UpdateRdv(context.Background(), gqlClient, newAppointmentId, oldAppointment.GetRdvById.Id_patient, appointment.GetRdvById.Doctor_id, appointment.GetRdvById.Start_date, appointment.GetRdvById.End_date, appointment.GetRdvById.Cancelation_reason, oldAppointment.GetRdvById.Appointment_status, oldAppointment.GetRdvById.Session_id)
+=======
+
+	updatedRdv, err := graphql.UpdateRdv(context.Background(), gqlClient, newAppointmentId, oldAppointment.GetRdvById.Id_patient, appointment.GetRdvById.Doctor_id, appointment.GetRdvById.Start_date, appointment.GetRdvById.End_date, appointment.GetRdvById.Cancelation_reason, appointment.GetRdvById.Appointment_status, appointment.GetRdvById.Session_id)
+>>>>>>> 8496ff2 (fix)
 	if err != nil {
 		return UpdateDoctorAppointmentStruct{Rdv: model.Rdv{}, Patient: model.Patient{}, Code: 500, Err: errors.New("unable to update appointment")}
 	}
@@ -42,8 +47,12 @@ func UpdateDoctorAppointment(newAppointmentId string, appointmentId string) Upda
 	if err != nil {
 		return UpdateDoctorAppointmentStruct{Rdv: model.Rdv{}, Patient: model.Patient{}, Code: 400, Err: errors.New("id does not correspond to an patient")}
 	}
+<<<<<<< HEAD
 	var appointment_status = graphql.AppointmentStatusOpened
 	_, err = graphql.UpdateRdv(context.Background(), gqlClient, appointmentId, "", oldAppointment.GetRdvById.Doctor_id, oldAppointment.GetRdvById.Start_date, oldAppointment.GetRdvById.End_date, oldAppointment.GetRdvById.Cancelation_reason, appointment_status, oldAppointment.GetRdvById.Session_id)
+=======
+	_, err = graphql.UpdateRdv(context.Background(), gqlClient, appointmentId, "", oldAppointment.GetRdvById.Doctor_id, oldAppointment.GetRdvById.Start_date, oldAppointment.GetRdvById.End_date, oldAppointment.GetRdvById.Cancelation_reason, oldAppointment.GetRdvById.Appointment_status, oldAppointment.GetRdvById.Session_id)
+>>>>>>> 8496ff2 (fix)
 	if err != nil {
 		return UpdateDoctorAppointmentStruct{Rdv: model.Rdv{}, Patient: model.Patient{}, Code: 500, Err: errors.New("unable to update appointment")}
 	}
