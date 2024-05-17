459d28c280a6f1cd8163bc33a2942c792eea90ea
ThomasDarrieumerlou <72026268+ThomasDarrieumerlou@users.noreply.github.com>
2024-05-17 16:29:21 +0100

    Messagerie (#91) (#92)
    * fix(compilation): missing update reset password
    
    * fix(doc): modify time
    
    * fix(document): modify privatisation function for s3

--- document/generate_url.go
+++ document/generate_url.go
@@ -1,15 +1,29 @@
 package document
 
 import (
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/joho/godotenv"
+	"os"
 	"time"
 )
 
 func generateSignedURL(bucketName, objectKey string) (string, error) {
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
+	err := godotenv.Load()
+	if err != nil {
+		return "", fmt.Errorf("erreur lors du chargement du fichier .env: %w", err)
+	}
+
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(os.Getenv("EDGAR_REGION")),
+		Credentials: credentials.NewStaticCredentials(
+			os.Getenv("EDGAR_ACCESS_KEY_ID"),
+			os.Getenv("EDGAR_SECRET_ACCESS_KEY"),
+			"",
+		),
 	}))
 
 	svc := s3.New(sess)
@@ -19,9 +33,9 @@ func generateSignedURL(bucketName, objectKey string) (string, error) {
 		Key:    aws.String(objectKey),
 	})
 
-	url, err := req.Presign(5 * time.Minute)
+	url, err := req.Presign(20 * time.Minute)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("erreur lors de la génération de l'URL signée: %w", err)
 	}
 
 	return url, nil
