0522a4cde787578189566f0224121fe56435e12c
ThomasDarrieumerlou <thomas.darrieumerlou@epitech.eu>
2024-05-18 16:45:29 +0100

    fix(prediagnostic): change value on return

--- document/generate_url.go
+++ document/generate_url.go
@@ -4,10 +4,11 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"net/url"
 	"time"
 )
 
-func generateURL(bucket string, key string) (string, error) {
+func generateURL(bucket string, key string, customFilename string) (string, error) {
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String("eu-west-3")},
 	)
@@ -21,6 +22,12 @@ func generateURL(bucket string, key string) (string, error) {
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
 	})
+
+	// Add custom filename as a query parameter
+	req.HTTPRequest.URL.RawQuery = url.Values{
+		"response-content-disposition": []string{"attachment; filename=\"" + customFilename + "\""},
+	}.Encode()
+
 	urlStr, err := req.Presign(15 * time.Minute)
 	if err != nil {
 		return "", err
