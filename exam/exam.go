12f4c6556b8f16de3b16b4860d9d87d1d399d0cd
LohicM <72015706+LohicM@users.noreply.github.com>
2024-09-06 16:36:31 +0200

    feat(simulation) add: Automatic answers added to diagnose

--- exam/exam.go
+++ exam/exam.go
@@ -5,22 +5,24 @@ import (
 )
 
 type ExamResponse struct {
-	Context  []model.SessionSymptom
-	Question string
-	Symptoms []string
-	Done     bool
-	Alert    []string
-	Code     int
-	Err      error
+	Context    []model.SessionSymptom
+	Question   string
+	Symptoms   []string
+	AutoAnswer *string
+	Done       bool
+	Alert      []string
+	Code       int
+	Err        error
 }
 
 func Exam(context []model.SessionSymptom, imc float64, anteChirIds []string, hereditary_disease []string) ExamResponse {
 	var question string
 	var possibleSymptoms []string
+	var autoA *string
 	mappedDiseaseCoverage, isDone := Calculi(context, imc, anteChirIds, hereditary_disease)
 	if isDone == false {
 		var err error
-		question, possibleSymptoms, err = GuessQuestion(mappedDiseaseCoverage)
+		question, autoA, possibleSymptoms, err = GuessQuestion(mappedDiseaseCoverage)
 		if err != nil {
 			return ExamResponse{Code: 500, Err: err}
 		}
@@ -30,12 +32,13 @@ func Exam(context []model.SessionSymptom, imc float64, anteChirIds []string, her
 		return ExamResponse{Code: 500, Err: err}
 	}
 	return ExamResponse{
-		Context:  context,
-		Question: question,
-		Symptoms: possibleSymptoms,
-		Done:     isDone,
-		Alert:    alert,
-		Code:     200,
-		Err:      nil,
+		Context:    context,
+		Question:   question,
+		Symptoms:   possibleSymptoms,
+		AutoAnswer: autoA,
+		Done:       isDone,
+		Alert:      alert,
+		Code:       200,
+		Err:        nil,
 	}
 }
