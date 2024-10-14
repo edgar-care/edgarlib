825a21a61de3b66e4062ed5088df2a7ccbfb69d0
ThomasDarrieumerlou <72026268+ThomasDarrieumerlou@users.noreply.github.com>
2024-10-14 15:00:08 +0200

    fix(medicine): add enum + change type for dosage int to float (#198)

--- appointment/book_appointment_test.go
+++ appointment/book_appointment_test.go
@@ -1,7 +1,9 @@
 package appointment
 
 import (
+	"github.com/edgar-care/edgarlib/v2/dashboard"
 	"github.com/edgar-care/edgarlib/v2/graphql/model"
+	"github.com/edgar-care/edgarlib/v2/medical_folder"
 	"github.com/joho/godotenv"
 	"log"
 	"testing"
@@ -154,4 +156,97 @@ func TestBookAppointmentInvalidDoctorId(t *testing.T) {
 	if response.Code != 400 || response.Err == nil {
 		t.Errorf("Expected error and code 400 but got code %d and err: %s", response.Code, response.Err.Error())
 	}
+}
+
+func TestBookAppointmentCheckDoctor(t *testing.T) {
+	if err := godotenv.Load(".env.test"); err != nil {
+		log.Fatalf("Error loading .env.test file: %v", err)
+	}
+
+	patient, err := graphql.CreatePatient(model.CreatePatientInput{Email: "test_appointment_check_dupli@edgar-sante.fr", Password: "password"})
+	if err != nil {
+		t.Errorf("Error creating patient: %v", err)
+	}
+
+	input := medical_folder.CreateMedicalInfoInput{
+		Name:            "test",
+		Firstname:       "test",
+		Birthdate:       0,
+		Sex:             "M",
+		Weight:          0,
+		Height:          0,
+		PrimaryDoctorID: "",
+		MedicalAntecedents: []medical_folder.CreateMedicalAntecedentInput{{
+			Name: "test",
+			Medicines: []medical_folder.CreateMedicineInput{medical_folder.CreateMedicineInput{
+				MedicineID: "test",
+				Period:     []string{"NOON"},
+				Day:        []string{"MONDAY"},
+				Quantity:   2,
+				StartDate:  1234,
+				EndDate:    1234,
+			}},
+			StillRelevant: false,
+		},
+		},
+		FamilyMembersMedInfoId: []string{"test"},
+	}
+
+	_ = medical_folder.CreateMedicalInfo(input, patient.ID)
+
+	doctor, err := graphql.CreateDoctor(model.CreateDoctorInput{Email: "test_doctor_appointment_check_dupli@edgar-sante.fr", Password: "password", Name: "name", Firstname: "first", Address: &model.AddressInput{"", "", "", ""}})
+	if err != nil {
+		t.Errorf("Error while creating doctor: %v", err)
+	}
+	patientID := patient.ID
+	appointment, err := graphql.CreateRdv(model.CreateRdvInput{
+		IDPatient:         "",
+		DoctorID:          doctor.ID,
+		StartDate:         0,
+		EndDate:           10,
+		AppointmentStatus: "OPENED",
+		SessionID:         "",
+	})
+	if err != nil {
+		t.Errorf("Error creating appointment: %v", err)
+	}
+	appointmentID := appointment.ID
+
+	response := BookAppointment(appointmentID, patientID, "session")
+
+	appointment2, err := graphql.CreateRdv(model.CreateRdvInput{
+		IDPatient:         "",
+		DoctorID:          doctor.ID,
+		StartDate:         45,
+		EndDate:           65,
+		AppointmentStatus: "OPENED",
+		SessionID:         "",
+	})
+	if err != nil {
+		t.Errorf("Error creating appointment: %v", err)
+	}
+	appointment2ID := appointment2.ID
+
+	_ = BookAppointment(appointment2ID, patientID, "session")
+
+	if response.Err != nil {
+		t.Errorf("Error booking appointment: %v", response.Err)
+	}
+
+	appointmentResult, err := graphql.GetRdvById(appointmentID)
+	if err != nil {
+		t.Errorf("Error getting appointment by ID: %v", err)
+	}
+
+	if appointmentResult.IDPatient != patientID {
+		t.Errorf("Appointment not booked correctly")
+	}
+
+	_, err = graphql.GetDoctorById(doctor.ID)
+	if err != nil {
+		t.Errorf("Error getting doctor by ID: %v", err)
+	}
+
+	_ = dashboard.GetPatients(doctor.ID)
+
 }
